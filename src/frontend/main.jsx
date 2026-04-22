ca0d50e86640bc10eb10a6654c8526605713b1e4
stanevko-ilya <stanevko.ilya@gmail.com>
2026-04-22 15:28:52 +0300

    [fix] use HashRouter when served from file://

--- src/frontend/main.jsx
+++ src/frontend/main.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 import App from './App';
 import './styles/historymap.css';
 import './styles/MapPage.css';
 
+const Router = window.location.protocol === 'file:' ? HashRouter : BrowserRouter;
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <Router>
       <App />
-    </BrowserRouter>
+    </Router>
   </React.StrictMode>
 );
